18f02dae2ba65d752c12183641c29b689b54958a
adriantecprogrammer <adrian2004mexico.com@gmail.com>
2023-12-20 20:22:56 -0600

    feat: 🚧 Se crearon las interfaces
    Se crearon las interfaces que albergan el crud repository para cada clase (Estado,CP,Municipio,Colonia)
    
    close

---
+++ Api_CodigosPostales/Api_CodigosPostales/src/main/java/com/apiCodigosPostales/model/entity/dao/EstadoDao.java
@@ -0,0 +1,7 @@
+package com.apiCodigosPostales.model.entity.dao;
+
+import io.micronaut.data.repository.CrudRepository;
+
+public interface EstadoDao extends CrudRepository<Estado, Integer> {
+
+}
---
+++ Api_CodigosPostales/Api_CodigosPostales/src/main/java/com/apiCodigosPostales/model/entity/dao/MunicipioDao.java
@@ -0,0 +1,6 @@
+package com.apiCodigosPostales.model.entity.dao;
+
+import io.micronaut.data.repository.CrudRepository;
+
+public interface MunicipioDao extends CrudRepository<Municipio, String> {
+}
